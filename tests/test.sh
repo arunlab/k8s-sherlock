54e2490337a1747b547d48e2fa2c748375256f0c
thisiz_A <arun.sanna@outlook.com>
2025-01-10 19:23:00 -0500

    Add kubectl and helm to Dockerfile
    Update Dockerfile to use latest Ubuntu image and add kubectl and helm installations.
    
    * **Dockerfile**
      - Change base image from `ubuntu:noble` to `ubuntu:latest`.
      - Add installation steps for `kubectl`.
      - Add installation steps for `helm`.
    
    * **tests/test.sh**
      - Modify container run command to run in detached mode.
      - Add tests to verify `kubectl` installation.
      - Add tests to verify `helm` installation.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/vivsoftorg/k8s-sherlock?shareId=XXXX-XXXX-XXXX-XXXX).

--- tests/test.sh
+++ tests/test.sh
@@ -8,12 +8,20 @@ docker build -t test-image .
 
 # Run the container
 echo "Running Docker container..."
-container_id=$(docker run -it --rm test-image:latest)
+container_id=$(docker run -d test-image:latest)
 
 # Helper function to run commands inside the container
 run_command() {
   docker exec $container_id "$@"
 }
+
+# Test kubectl installation
+echo "Testing kubectl installation..."
+run_command kubectl version --client
+
+# Test helm installation
+echo "Testing helm installation..."
+run_command helm version
 
 # All tests passed
 echo "All tests passed successfully."
